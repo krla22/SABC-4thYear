825b545dd3300c0b8c2465d0b317eecd8d12400b
krla22 <kurtrobin22@gmail.com>
2023-10-08 17:47:24 +0800

    Done

---
+++ scripts/products_slideshow.js
@@ -0,0 +1,35 @@
+function changeImg1() {
+  document.getElementById("sensnia-img").src = "images/sensnia-serum-1.jpg";
+}
+
+function changeImg2() {
+  document.getElementById("sensnia-img").src = "images/sensnia-serum-2.jpg";
+}
+
+function changeImg3() {
+  document.getElementById("sensnia-img").src = "images/sensnia-serum-info.jpg";
+}
+
+function changeImg4() {
+  document.getElementById("ub-img").src = "images/ub-serum-1.jpg";
+}
+
+function changeImg5() {
+  document.getElementById("ub-img").src = "images/ub-serum-2.jpg";
+}
+
+function changeImg6() {
+  document.getElementById("ub-img").src = "images/ub-serum-info.jpg";
+}
+
+function changeImg7() {
+  document.getElementById("sensoa-img").src = "images/oaprone-serum-1.jpg";
+}
+
+function changeImg8() {
+  document.getElementById("sensoa-img").src = "images/oaprone-serum-2.jpg";
+}
+
+function changeImg9() {
+  document.getElementById("sensoa-img").src = "images/oaprone--serum-info.jpg";
+}
